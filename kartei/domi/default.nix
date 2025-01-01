3032b42c884e626c47a0ce68984fd1e8fc5df810
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2025-01-01 20:22:37 +0100

    kartei: add domi

--- kartei/domi/default.nix
+++ kartei/domi/default.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
   domi = {
     nets.wiregrill = {
       ip4.addr = "10.244.10.108";
+      aliases = [
+        "domi.w"
+      ];
+      owner = config.krebs.users.domi;
       wireguard.pubkey = "Yy1pvM0lEwaXuOwBoFGNYhHeyYEKuR/rovE0/myWyCI=";
     };
   };
