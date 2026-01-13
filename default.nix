krebs/stockholm

commit 68d3c925b0edb958fcbafa589d35967dfb2a979c

5pkgs: fix nix flake show compatibility
Integrate nix-writers flake for extended writer functions (writeJq,
writeGawkBin, etc.) using a fixpoint overlay so functions can reference
each other.

Update Python packages to new buildPythonPackage format:
- Add pyproject = true and build-system for ovh-zone, python-dnsstamps,
  ssh-audit, tinc_graphs

Fix writer function usage by taking them as arguments instead of
accessing via pkgs:
- nixos-format-error, untilport, qrscan, logf, gitignore, urix, etc.

Update deprecated nixpkgs attribute names:
- gdk_pixbuf -> gdk-pixbuf
- libSM/libX11/libXinerama -> xorg.*
- buildGo120Module -> buildGoModule

Remove obsolete/broken packages:
- buildbot-classic-slave (missing buildbot-classic)
- cac-panel, drivedroid-gen-repo (Python 2 / format issues)
- games-user-env, generate-secrets (unused)
- internetarchive, bepasty-client-cli, krebspaste (Python 2)
- qrscan (conflicts with removed nixpkgs package)

Other fixes:
- Use lib.callPackageWith for package interdependencies
- Only export derivations in flake packages output
- Limit packages output to Linux systems
- Fix IFD in ukrepl by passing path to writer instead of readFile
- Import stockholm.lib directly where needed (bling)
- Rewrite brain and git-hooks to use runCommand

krebs/5pkgs/simple/default.nix
@@ -3,8 +3,9 @@ self: super:
 let
   # This callPackage will try to detect obsolete overrides.
   lib = super.stockholm.lib;
+  callPackage' = lib.callPackageWith self;
   callPackage = path: args: let
-    override = self.callPackage path args;
+    override = callPackage' path args;
     upstream = lib.optionalAttrs (override ? "name")
       (super.${(lib.parseDrvName override.name).name} or {});
   in if upstream ? "name" &&

krebs/5pkgs/simple/git-hooks/default.nix
@@ -1,129 +1,108 @@
-{ pkgs, stockholm, ... }:
+{ pkgs, lib, writeDash, ... }:
 
-with stockholm.lib;
+let
+  stockholm.lib = import ../../../../lib/pure.nix { inherit lib; };
+  inherit (stockholm.lib) makeBinPath;
+in
 
-{
-  # TODO irc-announce should return a derivation
-  #      but it cannot because krebs.git.repos.*.hooks :: attrsOf str
-  irc-announce =
-  { cgit_endpoint ? "http://cgit.${nick}.r"
-  , channel
-  , nick
-  , port ? 6667
-  , refs ? []
-  , server
-  , tls ? false
-  , verbose ? false
-  }: /* sh */ ''
-    #! /bin/sh
-    set -euf
+pkgs.runCommand "irc-announce-git-hook" {} ''
+  mkdir -p $out/bin
+  cat > $out/bin/irc-announce-git-hook << 'OUTER'
+#!${pkgs.dash}/bin/dash
+set -euf
 
-    export PATH=${makeBinPath (with pkgs; [
-      coreutils
-      git
-      gnugrep
-      gnused
-    ])}
+# Required environment variables:
+# IRC_SERVER, IRC_PORT, IRC_NICK, IRC_CHANNEL
+# Optional: IRC_TLS (set to "true" for TLS), CGIT_ENDPOINT, VERBOSE
 
-    green()  { printf '\x0303,99%s\x0F' "$1"; }
-    red()    { printf '\x0304,99%s\x0F' "$1"; }
-    orange() { printf '\x0307,99%s\x0F' "$1"; }
-    pink()   { printf '\x0313,99%s\x0F' "$1"; }
-    gray()   { printf '\x0314,99%s\x0F' "$1"; }
+export PATH=${makeBinPath (with pkgs; [
+  coreutils
+  git
+  gnugrep
+  gnused
+])}:$PATH
 
-    unset message
-    add_message() {
-      message="''${message+$message
-    }$*"
-    }
+green()  { printf '\x0303,99%s\x0F' "$1"; }
+red()    { printf '\x0304,99%s\x0F' "$1"; }
+orange() { printf '\x0307,99%s\x0F' "$1"; }
+pink()   { printf '\x0313,99%s\x0F' "$1"; }
+gray()   { printf '\x0314,99%s\x0F' "$1"; }
 
-    nick=${escapeShellArg nick}
-    channel=${escapeShellArg channel}
-    server=${escapeShellArg server}
-    tls=${escapeShellArg tls}
-    port=${toString port}
-
-    host=$nick
-
-    empty=0000000000000000000000000000000000000000
-
-    while read oldrev newrev ref; do
-
-      if [ $oldrev = $empty ]; then
-        receive_mode=create
-      elif [ $newrev = $empty ]; then
-        receive_mode=delete
-      elif [ "$(git merge-base $oldrev $newrev)" = $oldrev ]; then
-        receive_mode=fast-forward
-      else
-        receive_mode=non-fast-forward
-      fi
-
-      ${optionalString (refs != []) ''
-        if ! { echo "$ref" | grep -qE "${concatStringsSep "|" refs}"; }; then
-          echo "we are not announcing this ref: $h"
-          exit 0
-        fi
-      ''}
-
-      h=$(echo $ref | sed 's:^refs/heads/::')
-
-      # empty_tree=$(git hash-object -t tree /dev/null)
-      empty_tree=4b825dc6
-
-      id=$(echo $newrev | cut -b-7)
-      id2=$(echo $oldrev | cut -b-7)
-      if [ $newrev = $empty ]; then id=$empty_tree; fi
-      if [ $oldrev = $empty ]; then id2=$empty_tree; fi
-
-      ${if cgit_endpoint != null then /* sh */ ''
-        cgit_endpoint=${escapeShellArg cgit_endpoint}
-        case $receive_mode in
-          create)
-            link="$cgit_endpoint/$GIT_SSH_REPO/?h=$h"
-            ;;
-          delete)
-            link="$cgit_endpoint/$GIT_SSH_REPO/ ($h)"
-            ;;
-          fast-forward|non-fast-forward)
-            link="$cgit_endpoint/$GIT_SSH_REPO/diff/?h=$h&id=$id&id2=$id2"
-            ;;
-        esac
-      '' else /* sh */ ''
-        link="$GIT_SSH_REPO $h"
-      ''}
-
-      #$host $GIT_SSH_REPO $ref $link
-      add_message $(pink push) $link $(gray "($receive_mode)")
-
-      ${optionalString (verbose == true || typeOf verbose == "set") /* sh */ ''
-        ${optionalString (verbose.exclude or [] != []) /* sh */ ''
-          case $ref in (${concatStringsSep "|" verbose.exclude})
-            continue
-          esac
-        ''}
-        add_message "$(
-          git log \
-              --format="$(orange %h) %s $(gray '(%ar)')" \
-              --no-merges \
-              --reverse \
-              $id2..$id
-
-          git diff --stat $id2..$id \
-            | sed '$!s/\(+*\)\(-*\)$/'$(green '\1')$(red '\2')'/'
-        )"
-      ''}
-
-    done
-
-    if test -n "''${message-}"; then
-      exec ${pkgs.irc-announce}/bin/irc-announce \
-        "$server" \
-        "$port" \
-        "$nick" \
-        "$channel" \
-        "$tls" \
-        "$message"
-    fi
-  '';
+unset message
+add_message() {
+  message="''${message+$message
+}$*"
 }
+
+empty=0000000000000000000000000000000000000000
+
+while read oldrev newrev ref; do
+
+  if [ $oldrev = $empty ]; then
+    receive_mode=create
+  elif [ $newrev = $empty ]; then
+    receive_mode=delete
+  elif [ "$(git merge-base $oldrev $newrev)" = $oldrev ]; then
+    receive_mode=fast-forward
+  else
+    receive_mode=non-fast-forward
+  fi
+
+  h=$(echo $ref | sed 's:^refs/heads/::')
+
+  empty_tree=4b825dc6
+
+  id=$(echo $newrev | cut -b-7)
+  id2=$(echo $oldrev | cut -b-7)
+  if [ $newrev = $empty ]; then id=$empty_tree; fi
+  if [ $oldrev = $empty ]; then id2=$empty_tree; fi
+
+  if [ -n "''${CGIT_ENDPOINT:-}" ]; then
+    case $receive_mode in
+      create)
+        link="$CGIT_ENDPOINT/$GIT_SSH_REPO/?h=$h"
+        ;;
+      delete)
+        link="$CGIT_ENDPOINT/$GIT_SSH_REPO/ ($h)"
+        ;;
+      fast-forward|non-fast-forward)
+        link="$CGIT_ENDPOINT/$GIT_SSH_REPO/diff/?h=$h&id=$id&id2=$id2"
+        ;;
+    esac
+  else
+    link="$GIT_SSH_REPO $h"
+  fi
+
+  add_message $(pink push) $link $(gray "($receive_mode)")
+
+  if [ "''${VERBOSE:-}" = "true" ]; then
+    add_message "$(
+      git log \
+          --format="$(orange %h) %s $(gray '(%ar)')" \
+          --no-merges \
+          --reverse \
+          $id2..$id
+
+      git diff --stat $id2..$id \
+        | sed '$!s/\(+*\)\(-*\)$/'"$(green '\1')$(red '\2')"'/'
+    )"
+  fi
+
+done
+
+if test -n "''${message-}"; then
+  tls_flag=""
+  if [ "''${IRC_TLS:-}" = "true" ]; then
+    tls_flag="1"
+  fi
+  exec ${pkgs.irc-announce}/bin/irc-announce \
+    "$IRC_SERVER" \
+    "$IRC_PORT" \
+    "$IRC_NICK" \
+    "$IRC_CHANNEL" \
+    "$tls_flag" \
+    "$message"
+fi
+OUTER
+  chmod +x $out/bin/irc-announce-git-hook
+''

krebs/5pkgs/simple/gitignore.nix
@@ -1,4 +1,4 @@
-{ pkgs }:
+{ pkgs, writeDash }:
 
 /* gitignore - Filter for intentionally untracked lines or blocks of code
 
@@ -34,13 +34,9 @@ Installation:
   [2]: For more information about assigning filters see gitattributes(5).
 */
 
-pkgs.execBin "gitignore" {
-  filename = "${pkgs.gnused}/bin/sed";
-  argv = [
-    "gitignore"
-    /* sed */ ''
-      /#gitignore-begin/,/#gitignore-end/d
-      /#gitignore/d
-    ''
-  ];
-}
+writeDash "gitignore" ''
+  exec ${pkgs.gnused}/bin/sed '
+    /#gitignore-begin/,/#gitignore-end/d
+    /#gitignore/d
+  '
+''

krebs/5pkgs/default.nix
@@ -8,7 +8,10 @@ with stockholm.lib;
 
 fix (foldl' (flip extends) (self: super) (
   [
-    (self: super: { inherit stockholm; })
+    (self: super: {
+      inherit stockholm;
+      inherit (super.writers) writeBash writeBashBin writeDash writeDashBin writeJSON;
+    })
   ]
   ++
   (map

krebs/5pkgs/simple/bepasty-client-cli/default.nix
@@ -1,23 +1,0 @@
-{ lib, pkgs, pythonPackages, fetchFromGitHub, ... }:
-
-with pythonPackages; buildPythonPackage rec {
-  name = "bepasty-client-cli";
-  propagatedBuildInputs = [
-    python_magic
-    click
-    requests
-  ];
-
-  src = fetchFromGitHub {
-    owner = "bepasty";
-    repo = "bepasty-client-cli";
-    rev = "4b7135ba8ba1e17501de08ad7b6aca73c0d949d2";
-    sha256 = "1svchyk9zai1vip9ppm12jm7wfjbdr9ijhgcd2n10xh73jrn9cnc";
-  };
-
-  meta = {
-    homepage = https://github.com/bepasty/bepasty-client-cli;
-    description = "CLI client for bepasty-server";
-    license = lib.licenses.bsd2;
-  };
-}

krebs/5pkgs/simple/bling/default.nix
@@ -1,4 +1,7 @@
-{ imagemagick, runCommand, stockholm, ... }:
+{ imagemagick, runCommand, lib, ... }:
+let
+  stockholm.lib = import ../../../../lib/pure.nix { inherit lib; };
+in
 with stockholm.lib;
 
 let

krebs/5pkgs/simple/brain/default.nix
@@ -4,25 +4,30 @@ let
   pass = pkgs.pass.withExtensions (ext: [
     ext.pass-otp
   ]);
-in
 
-pkgs.write "brain" {
-  "/bin/brain".link = pkgs.writeDash "brain" ''
+  brain = pkgs.writeDash "brain" ''
     PASSWORD_STORE_DIR=$HOME/brain \
     exec ${pass}/bin/pass "$@"
   '';
-  "/bin/brainmenu".link = pkgs.writeDash "brainmenu" ''
+
+  brainmenu = pkgs.writeDash "brainmenu" ''
     PASSWORD_STORE_DIR=$HOME/brain \
     exec ${pass}/bin/passmenu "$@"
   '';
-  "/share/bash-completion/completions/brain".link =
-    pkgs.runCommand "brain-completions" {
-    } /* sh */ ''
-      sed -r '
-        s/\<_pass?(_|\>)/_brain\1/g
-        s/\<__password_store/_brain/g
-        s/\<pass\>/brain/
-        s/\$HOME\/\.password-store/$HOME\/brain/
-      ' < ${pass}/share/bash-completion/completions/pass > $out
-    '';
-}
+
+  completions = pkgs.runCommand "brain-completions" {} ''
+    sed -r '
+      s/\<_pass?(_|\>)/_brain\1/g
+      s/\<__password_store/_brain/g
+      s/\<pass\>/brain/
+      s/\$HOME\/\.password-store/$HOME\/brain/
+    ' < ${pass}/share/bash-completion/completions/pass > $out
+  '';
+in
+
+pkgs.runCommand "brain" {} ''
+  mkdir -p $out/bin $out/share/bash-completion/completions
+  ln -s ${brain} $out/bin/brain
+  ln -s ${brainmenu} $out/bin/brainmenu
+  ln -s ${completions} $out/share/bash-completion/completions/brain
+''

krebs/5pkgs/simple/buildbot-classic-slave/default.nix
@@ -1,18 +1,0 @@
-{ coreutils, fetchgit, git, buildbot-classic, python2Packages, ... }:
-
-python2Packages.buildPythonApplication {
-  name = "buildbot-classic-slave-${buildbot-classic.version}";
-  namePrefix = "";
-
-  src = buildbot-classic.src;
-  postUnpack = "sourceRoot=\${sourceRoot}/slave";
-
-  nativeBuildInputs = [ git ];
-  propagatedBuildInputs = [ python2Packages.twisted ];
-  doCheck = false;
-
-  postInstall = ''
-    mkdir -p "$out/share/man/man1"
-    cp docs/buildslave.1 "$out/share/man/man1"
-  '';
-}

commit 30be9ada2486b92c39ddc464676f028dce980b55

gitignore: fix bug introduced by 68d3c92

default.nix
@@ -1,7 +1,7 @@
 import <nixpkgs/nixos> {} // rec {
   lib = import ./lib;
   systems = with lib; let
-    namespace = getEnv "LOGNAME";
+    namespace = krebs;
     systemsDir = <stockholm> + "/${namespace}/1systems";
   in
     genAttrs

krebs/1systems/ponte/config.nix
@@ -8,6 +8,18 @@
     <stockholm/krebs/2configs/nameserver.nix>
   ];
 
+  #networking.defaultGateway6 = {
+  #  address = "fe80::1";
+  #  interface = "ens3";
+  #};
+  #networking.interfaces.ens3.ipv6.addresses = [
+  #  {
+  #    # old: address = "2a03:4000:13:4c::1";
+  #    address = "2a03:4000:1a:cf::1"; #/64"
+  #    prefixLength = 64;
+  #  }
+  #];
+
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.firewall.logRefusedConnections = false;
   networking.firewall.logRefusedUnicastsOnly = false;

krebs/2configs/nameserver.nix
@@ -146,10 +146,14 @@ in {
     '';
   };
 
+  #krebs.systemd.services.knot.restartIfCredentialsChange = true;
   systemd.services."knsupdate-krebsco.de" = {
     serviceConfig = {
       Type = "oneshot";
       SyslogIdentifier = "knsupdate-krebsco.de";
+      #LoadCredential = [
+      #  "keys.conf:/var/src/secrets/knot-keys.conf"
+      #];
       ExecStart = pkgs.writeDash "knsupdate-krebsco.de" /* sh */ ''
         set -efu
 

krebs/3modules/github/known-hosts.nix
@@ -8,4 +8,5 @@
     ;
     publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=";
   };
+  # ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
 }

krebs/3modules/hosts.nix
@@ -2,7 +2,6 @@
 with lib; let
   check = hostname: any (domain: hasSuffix ".${domain}" hostname) domains;
   domains = attrNames (filterAttrs (_: slib.eq "hosts") config.krebs.dns.providers);
-  # we need this import because we have infinite recursion otherwise
   slib = lib.slib or (import ../../lib/pure.nix { inherit lib; });
 in {
 

krebs/3modules/iptables.nix
@@ -19,6 +19,14 @@ let
   api = {
     enable = mkEnableOption "iptables";
 
+    rules4 = mkOption {
+      default = buildTables "v4" cfg.tables;
+    };
+
+    rules6 = mkOption {
+      default = buildTables "v6" cfg.tables;
+    };
+
     #tables.filter.INPUT = {
     # policy = "DROP";
     # rules = [

krebs/3modules/per-user.nix
@@ -28,7 +28,12 @@ in {
             };
           })
           (filterAttrs (_: per-user: per-user.packages != []) cfg);
-      profiles = ["/etc/per-user/$LOGNAME"];
+
+      # XXX this breaks /etc/pam/environment because $LOGNAME doesn't get
+      # replaced by @{PAM_USER} the way $USER does.
+      # See <nixpkgs/nixos/modules/config/system-environment.nix>
+      #profiles = ["/etc/per-user/$LOGNAME"];
+      profiles = ["/etc/per-user/$USER"];
     };
   };
 }

krebs/3modules/permown.nix
@@ -73,6 +73,12 @@ with lib; {
           pkgs.findutils
           pkgs.inotify-tools
         ];
+        # TODO
+        # der code könnte aber bisschen vorbereitet werden, damit man später einfach file-modes einbauen kann
+        # die drei finds müssten zu `find "$ROOT_PATH" -exec ${permown}` {} \;` werden
+        # und der while-block zu:
+        # ${permown} "$path" (egal ob vor oder nach dem if test -d)
+        # und dann müsste man danach nur das permown script bearbeiten
         serviceConfig = {
           ExecStart = pkgs.writeDash "permown" ''
             set -efu

krebs/3modules/reaktor2.nix
@@ -69,6 +69,12 @@ with lib.slib or (import ../../lib/pure.nix { inherit lib; }); {
           Group = "reaktor2";
           DynamicUser = true;
           StateDirectory = cfg.username;
+          #ExecStartPre = [
+          #  (pkgs.writeDash "test-dynamic-user" ''
+          #    set -efux
+          #    ${pkgs.coreutils}/bin/id
+          #  '')
+          #];
           ExecStart = let
             configFile = pkgs.writers.writeJSON configFileName configValue;
             configFileName = "${cfg.systemd-service-name}.config.json";

krebs/3modules/setuid.nix
@@ -33,7 +33,7 @@ let
         };
         capabilities = mkOption {
           default = [];
-          type = types.listOf types.str;
+          type = types.listOf types.str; # TODO
         };
         owner = mkOption {
           default = "root";
@@ -52,6 +52,8 @@ let
             merge = mergeOneOption;
           };
         };
+        # TODO clear non-standard wrapperDirs
+        # TODO? allow only wrapperDirs below /run/wrappers?
         wrapperDir = mkOption {
           default = config.security.wrapperDir;
           type = types.absolute-pathname;
@@ -73,13 +75,16 @@ let
         chown ${cfg.owner}:${cfg.group} ${dst}
         chmod ${cfg.mode} ${dst}
         ${optionalString (cfg.capabilities != []) /* sh */ ''
+          set -x
           ${pkgs.libcap.out}/bin/setcap ${concatMapStringsSep "," shell.escape cfg.capabilities} ${dst}
+          set +x
         ''}
       '';
     }));
   };
 
   imp = {
+    # run after "wrappers" so config.security.wrapperDir can be hijacked.
     systemd.services."krebs.setuid" = {
       wantedBy = [ "suid-sgid-wrappers.service" ];
       after = [ "suid-sgid-wrappers.service" ];

krebs/3modules/ssh.nix
@@ -62,24 +62,26 @@ let
               }
             ]));
 
-      programs.ssh.extraConfig = concatMapStrings
-        (net: ''
-          Host ${toString (net.aliases ++ net.addrs)}
-            Port ${toString net.ssh.port}
-        '')
-        (filter
-          (net: net.ssh.port != 22)
-          (concatMap (host: attrValues host.nets)
-            (mapAttrsToList
-              (_: host: recursiveUpdate host
-                (optionalAttrs (cfg.dns.search-domain != null &&
-                                hasAttr cfg.dns.search-domain host.nets) {
-                  nets."" = host.nets.${cfg.dns.search-domain} // {
-                    aliases = [host.name];
-                    addrs = [];
-                  };
-                }))
-              config.krebs.hosts)));
+      programs.ssh.extraConfig =
+        mkBefore/*<-KILLME*/
+          (concatMapStrings
+            (net: ''
+              Host ${toString (net.aliases ++ net.addrs)}
+                Port ${toString net.ssh.port}
+            '')
+            (filter
+              (net: net.ssh.port != 22)
+              (concatMap (host: attrValues host.nets)
+                (mapAttrsToList
+                  (_: host: recursiveUpdate host
+                    (optionalAttrs (cfg.dns.search-domain != null &&
+                                    hasAttr cfg.dns.search-domain host.nets) {
+                      nets."" = host.nets.${cfg.dns.search-domain} // {
+                        aliases = [host.name];
+                        addrs = [];
+                      };
+                    }))
+                  config.krebs.hosts))));
     }
   ];
 

krebs/3modules/tinc.nix
@@ -1,5 +1,6 @@
-{ config, pkgs, lib, ... }:
-with lib.slib or (import ../../lib/pure.nix { inherit lib; }); {
+{ config, pkgs, lib, ... }: let
+  slib = lib.slib or (import ../../lib/pure.nix { inherit lib; });
+in with slib; {
   options.krebs.tinc = mkOption {
     default = {};
     description = ''
@@ -235,13 +236,14 @@ with lib.slib or (import ../../lib/pure.nix { inherit lib; }); {
               "$CREDENTIALS_DIRECTORY"/rsa_key.priv \
               /etc/tinc/${netname}/
         '';
-        ExecStart = "+" + toString [
-          "${cfg.tincPackage}/sbin/tincd"
-          "-D"
-          "-U ${cfg.username}"
-          "-d 0"
-          "-n ${netname}"
-        ];
+        ExecStart = "+" + pkgs.writers.writeDash "tinc-${netname}" ''
+          set -efu
+          exec ${cfg.tincPackage}/sbin/tincd \
+              -D \
+              -U ${cfg.username} \
+              -d 0 \
+              -n ${netname}
+        '';
         SyslogIdentifier = netname;
         DynamicUser = true;
         User = cfg.username;

krebs/3modules/urlwatch.nix
@@ -214,6 +214,7 @@ let
       };
       filter = mkOption {
         default = null;
+        # TODO nullOr subtypes.filter
         type =
           with types;
           nullOr (either str (listOf (pkgs.formats.json {}).type));

krebs/3modules/zones.nix
@@ -10,7 +10,8 @@ with lib; {
     default = {
       "krebsco.de" = /* bindzone */ ''
         $TTL 60
-        @ 3600 IN SOA spam.krebsco.de. spam.krebsco.de. 0 7200 3600 86400 3600
+        @ IN SOA spam.krebsco.de. spam.krebsco.de. 0 7200 3600 86400 3600
+
         @ 3600 IN NS ns1
         @ 3600 IN NS ni
         @ 3600 IN NS ns2.he.net.

krebs/5pkgs/simple/airdcpp-webclient/default.nix
@@ -4,7 +4,7 @@
 stdenv.mkDerivation rec {
   name = "airdcpp-webclient-${version}";
   version = "2.3.0";
-  
+
   src = fetchurl {
     url = http://web-builds.airdcpp.net/stable/airdcpp_2.3.0_webui-2.3.0_64-bit_portable.tar.gz;
     sha256 = "0yvcl0nc70fghc7vfsgvbpryi5q97arld8adql4way4qa0mdnyv1";

krebs/5pkgs/simple/default.nix
@@ -13,5 +13,15 @@ let
         lib.compareVersions upstream.name override.name != -1
     then lib.trace "Upstream `${upstream.name}' gets overridden by `${override.name}'." override
     else override;
+
+  ## This callPackage will try to detect obsolete overrides.
+  #callPackage = path: args: let
+  #  pname = (parseDrvName override.name).name;
+  #  override = self.callPackage path args;
+  #  upstream = super.${pname} or { name = ""; };
+  #in
+  #  override.overrideAttrs (old: {
+  #    name = warnOldVersion upstream.name old.name;
+  #  });
 in
   lib.mapNixDir (path: callPackage path {}) ./.

krebs/5pkgs/simple/font-size.nix
@@ -1,3 +1,4 @@
+# TODO kill me in favor of urxvt-resize-font
 { writeDashBin }:
 writeDashBin "font-size" ''
   set -efu

krebs/5pkgs/simple/git-hooks/default.nix
@@ -114,6 +114,7 @@ with stockholm.lib;
           git diff --stat $id2..$id \
             | sed '$!s/\(+*\)\(-*\)$/'$(green '\1')$(red '\2')'/'
         )"
+        echo "$(red "$message")"
       ''}
 
     done

krebs/5pkgs/simple/gitignore.nix
@@ -1,4 +1,4 @@
-{ pkgs, writeDash }:
+{ pkgs, writeDashBin }:
 
 /* gitignore - Filter for intentionally untracked lines or blocks of code
 
@@ -34,7 +34,7 @@ Installation:
   [2]: For more information about assigning filters see gitattributes(5).
 */
 
-writeDash "gitignore" ''
+writeDashBin "gitignore" ''
   exec ${pkgs.gnused}/bin/sed '
     /#gitignore-begin/,/#gitignore-end/d
     /#gitignore/d

krebs/5pkgs/simple/htgen-imgur/src/htgen-imgur
@@ -1,3 +1,4 @@
+# TODO dedup with paste
 find_item() {
   if test ${#1} -ge 7; then
     set -- "$(find "$STATEDIR/items" -mindepth 1 -maxdepth 1 \
@@ -58,6 +59,9 @@ read_uri() {
     $uri |
     capture("^((?<scheme>[^:]*):)?(//(?<authority>[^/]*))?(?<path>[^?#]*)([?](?<query>[^#]*))?([#](?<fragment>.*))?$") |
     . + {
+      #authority: (.authority | if . != null then
+      #  capture("^((?<userinfo>[^@]*)@)?(?<host>[^:]*)(:(?<port>.*))?$")
+      #else . end),
       query: (.query | if . != null then
         split("&") |
         map(split("=") | {key:.[0],value:.[1]}) |
@@ -141,26 +145,26 @@ case "$Method $path" in
           description: $uri.query.description,
           datetime: now,
           type: $info["MIME type"],
-          animated: false,
+          animated: false, # TODO
           width: $info.width,
           height: $info.height,
           size: $info.size,
-          views: 0,
-          bandwidth: 0,
-          vote: null,
-          favorite: false,
-          nsfw: null,
-          section: null,
-          account_url: null,
-          acount_id: 0,
-          is_ad: false,
-          is_most_viral: false,
-          tags: [],
-          ad_type: 0,
-          ad_url: "",
-          in_gallery: false,
+          views: 0, # TODO
+          bandwidth: 0, # TODO
+          vote: null, # TODO
+          favorite: false, # TODO
+          nsfw: null, # TODO
+          section: null, # TODO
+          account_url: null, # TODO
+          acount_id: 0, # TODO
+          is_ad: false, # TODO
+          is_most_viral: false, # TODO
+          tags: [], # TODO
+          ad_type: 0, # TODO
+          ad_url: "", # TODO
+          in_gallery: false, # TODO
           deletehash: @uri "\($id)?deletehash=\($deletehash)",
-          name: "",
+          name: "", # TODO
           link: $link,
         }
       ')

lib/default.nix
@@ -1,1 +1,0 @@
-import ./impure.nix

lib/eval-source.nix
@@ -1,4 +1,5 @@
-with import <stockholm/lib>;
+{ lib }:
+with lib;
 let
   eval = _file: source: evalModules {
     modules = singleton {

lib/impure.nix
@@ -1,3 +1,0 @@
-import ./pure.nix {
-  lib = import <nixpkgs/lib>;
-}

lib/pure.nix
@@ -9,7 +9,7 @@ let
       };
     in filterAttrsRecursive (name: _: !hasPrefix "_" name) eval.config;
 
-    evalSource = import ./eval-source.nix;
+    evalSource = import ./eval-source.nix { lib = stockholm.lib; };
 
     evalSubmodule = submodule: modules: let
       prefix = ["evalSubmodule"];
@@ -37,7 +37,7 @@ let
 
     eq = x: y: x == y;
     ne = x: y: x != y;
-    mod = x: y: x - y * (x / y);
+    #mod = x: y: x - y * (x / y);
 
     on = b: u: x: y: b (u x) (u y);
 

lib/types.nix
@@ -1,5 +1,4 @@
 { lib, ... }:
-
 let
   inherit (lib)
     all any attrNames concatMapStringsSep concatStringsSep const filter flip
@@ -198,9 +197,32 @@ rec {
       };
       tinc = mkOption {
         type = let net = config; in nullOr (submodule ({ config, ... }: {
+          config = {
+            config = 
+              #assert (with builtins; trace "xxxxxx ${toJSON config.subnets}" true);
+              concatStringsSep "\n" (
+                (optionals (net.via != null)
+                  (map (a: "Address = ${a} ${toString config.port}") net.via.addrs))
+                ++
+                (map (a: "Subnet = ${a}") config.subnets)
+                ++
+                (map (a: "Subnet = ${a}") net.addrs)
+                ++
+                [config.extraConfig]
+                ++
+                [config.pubkey]
+                ++
+                optional (config.pubkey_ed25519 != null) ''
+                  Ed25519PublicKey = ${config.pubkey_ed25519}
+                ''
+                ++
+                optional (config.weight != null) "Weight = ${toString config.weight}"
+              );
+          };
           options = {
             config = mkOption {
               type = str;
+              # TODO: readOnly = true;
               default = concatStringsSep "\n" (
                 (optionals (net.via != null)
                   (map (a: "Address = ${a} ${toString config.port}") net.via.addrs))